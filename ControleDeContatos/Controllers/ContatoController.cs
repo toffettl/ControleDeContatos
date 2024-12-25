6018eab556c50aa98bec8e986d1d8785f38514ec
felip <felip@DESKTOP-K14QK8H>
2024-12-25 20:00:27 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly ControleDeContatosContext _context;
+
+        public ContatoController(ControleDeContatosContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Contato
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ ControleDeContatos/Data/ControleDeContatosContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Data
+{
+    public class ControleDeContatosContext : DbContext
+    {
+        public ControleDeContatosContext (DbContextOptions<ControleDeContatosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ControleDeContatos.Models.ContatoModel> ContatoModel { get; set; } = default!;
+    }
+}
